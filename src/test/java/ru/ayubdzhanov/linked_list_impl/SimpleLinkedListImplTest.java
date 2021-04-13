ac8a4f3c43d21496277a3557b3e01597a0eac339
Егор <egorayudzhanov@gmail.com>
2021-04-13 20:59:33 +0300

    added tests

--- src/test/java/ru/ayubdzhanov/linked_list_impl/SimpleLinkedListImplTest.java
+++ src/test/java/ru/ayubdzhanov/linked_list_impl/SimpleLinkedListImplTest.java
@@ -1,7 +1,42 @@
 package ru.ayubdzhanov.linked_list_impl;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SimpleLinkedListImplTest {
 
+    @Test
+    public void addTest(){
+        LinkedList list = new SimpleLinkedListImpl();
+        list.add(1);
+        assertEquals(1, list.get(0));
+    }
+
+    @Test
+    public void removeTest(){
+        LinkedList list = new SimpleLinkedListImpl();
+        list.add(1);
+        assertEquals(1, list.get(0));
+        list.remove();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void removeTest2(){
+        LinkedList list = new SimpleLinkedListImpl();
+        list.add(5);
+        list.add(10);
+        list.remove(10);
+        assertFalse(list.find(10));
+    }
+
+    @Test
+    public void findTest(){
+        LinkedList list = new SimpleLinkedListImpl();
+        list.add(5);
+        list.add(10);
+        assertFalse(list.find(15));
+        assertTrue(list.find(5));
+    }
 }
